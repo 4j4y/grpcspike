0d51c7f5f50632836358203057db7f455cbb811c
Ajay Kumar Saini <ajay.k@go-jek.com>
2017-09-22 13:27:02 +0530

    Updated server to never shutdown

--- src/main/java/SpikeGRPCServer.java
+++ src/main/java/SpikeGRPCServer.java
@@ -1,9 +1,14 @@
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import spikegrpc.GreetingMessage;
+import spikegrpc.Name;
 import spikegrpc.SpikeGRPCGrpc;
-
 import java.io.IOException;
 
+/**
+ * Simple Implementaion of gRPC Server and service(s) to fulfill the request from client
+ */
 public class SpikeGRPCServer {
 
     private final int port;
@@ -15,26 +20,32 @@ public class SpikeGRPCServer {
         this.server = serverBuilder.build();
     }
 
-    public void start() throws IOException {
+    public void start() throws IOException, InterruptedException {
         server.start();
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
             public void run() {
                 if (SpikeGRPCServer.this.server != null) {
                     SpikeGRPCServer.this.server.shutdown();
+                    System.out.println("Server has shutdown");
                 }
             }
         });
+        server.awaitTermination();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         SpikeGRPCServer spikeGRPCServer = new SpikeGRPCServer(8980);
+        System.out.println("Server is Running");
         spikeGRPCServer.start();
     }
 
     private static class SpikeGRPCService extends SpikeGRPCGrpc.SpikeGRPCImplBase {
-
-
+        @Override
+        public void getHello(Name request, StreamObserver<GreetingMessage> responseObserver) {
+            responseObserver.onNext(GreetingMessage.newBuilder().setMessage("Hello "+request.getName()).build());
+            responseObserver.onCompleted();
+        }
     }
 
 }
